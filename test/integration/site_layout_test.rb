53cf960dd4b683f76d221fbbf6ca94808dfb9095
Stefano <sgazzotti@plum.com.ar>
2022-10-25 15:40:33 -0300

    Add integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_url
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
