d54ba5b29b0348104793f251b9e2a851ab090c1c
Stefano <sgazzotti@plum.com.ar>
2022-11-02 17:30:59 -0300

    Fix to authenticate password in session#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   def create
     login_username = params[:session][:username]
     login_password = params[:session][:password]
-    user = User.where("username = :query OR email = :query", query: login_username.downcase)
+    user = User.where("username = :query OR email = :query", query: login_username.downcase).first
     if user && user.authenticate(login_password)
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      flash[:danger] = 'Invalid username/email or password combination' # Can be better
+      render 'new', status: :unprocessable_entity
     end
-    render 'new', status: :unprocessable_entity
   end
 
   def destroy
