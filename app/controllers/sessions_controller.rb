d8ae3365d98783c3c22a9e9822aa10cf626b4616
Stefano <sgazzotti@plum.com.ar>
2022-11-03 16:30:23 -0300

    Add log in functionality Add log in test Add session temp cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,26 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    login_username = params[:session][:username]
+    login_username = params[:session][:username].downcase
     login_password = params[:session][:password]
-    user = User.where("username = :query OR email = :query", query: login_username.downcase).first
+    user = User.where("username = :query OR email = :query", query: login_username).first
     if user && user.authenticate(login_password)
-      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = 'Invalid username/email or password combination' # Can be better
+      flash.now[:danger] = 'Invalid email or username and password combination' # Can be better
       render 'new', status: :unprocessable_entity
     end
   end
 
   def destroy
   end
-
-  # private
-  #   def authenticate_email(email)
-  #     user = User.find_by(email: email.downcase).or(username: email)
-  #   end
-
-  #   def authenticate_username(username)
-  #     user = User.find_by(username: username.downcase)
-  #   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+
+	# Logs in the giver user.
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	# Looks for the user
+	def current_user
+		@current_user = @current_user || User.find_by(id: session[:user_id]) # if current_user is nil, then executes the User method
+	end
+
 end
