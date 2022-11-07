93aa8a9fee8a118cb17e7e8b465d8bbd09b1ebc9
Stefano <sgazzotti@plum.com.ar>
2022-11-07 11:34:21 -0300

    Add js and css to implement mobile responsive

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,10 +2,19 @@
 
 //Add toggle listeners to listen for clicks.
 document.addEventListener("turbo:load", () => {
+	let hamburger = document.querySelector("#hamburger");
+	hamburger.addEventListener("click", event => {
+		event.preventDefault();
+		let menu = document.querySelector("#navbars-menu");
+		menu.classList.toggle("collapse")
+	})
+})
+
+document.addEventListener("turbo:load", () => {
 	let account = document.querySelector("#account");
 	account.addEventListener("click", event => {
 		event.preventDefault();
 		let menu = document.querySelector("#dropdown-menu");
-		menu.classList.toggle("active");
+		menu.classList.toggle("active")
 	})
 })
