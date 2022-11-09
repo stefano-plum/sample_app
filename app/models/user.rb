5c9b76f1847d011dc30d2aa43467cc65dd2a34e4
Stefano <sgazzotti@plum.com.ar>
2022-11-09 12:34:37 -0300

    Add remember_token attribute to User class Add remember_digest column to User table on databa through migration Add new_token method which generates a random string Add remember method to User class to set a remember_token and update column in database Refactor User class digest and new_token methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,43 @@
 class User < ApplicationRecord
+
     VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     VALID_USERNAME_REGEX = /\A[a-z0-9_]{4,16}\z/i
+    attr_accessor :remember_token
     before_save :to_dwcase
+    # Password
+    has_secure_password
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, 
             format: { with: VALID_EMAIL_REGEX }, 
             uniqueness: true
     validates :username, presence: true, length: { maximum: 50 },
             format: { with: VALID_USERNAME_REGEX } ,uniqueness: true
-            
-    # Password
-    has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-    # Preguntar porque en el tutorial no valida el password_confirmation, vale la pena validarlo?
-    validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+    class << self
+
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
+                                                          BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+
+        def new_token
+            SecureRandom.urlsafe_base64
+        end
+        
+    end
     
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
-                                                      BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
 
     private
     
         def to_dwcase
-            email.downcase!
-            username.downcase!
+            self.email.downcase!
+            self.username.downcase!
         end
 end
 
