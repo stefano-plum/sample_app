e2fc391e36549d897ef6b963915fe7ca584d3774
Stefano <sgazzotti@plum.com.ar>
2022-11-18 15:51:02 -0300

    Add faker gem to gemfile Implement fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create a main sample user.
+User.create!(name: "Example User",
+             username: "ExampleUser",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+# Generate a bunch of additional users.
+
+99.times do |n|
+  Faker::Config.locale = :en
+  first_name = Faker::Name.first_name
+  last_name  = Faker::Name.last_name
+  name = "#{first_name} #{last_name}"
+  username = "#{first_name}#{last_name}"
+  puts username
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+               username: username,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
