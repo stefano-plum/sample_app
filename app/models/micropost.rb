3a9b62fa53d7348e3eb75f9c1d2f28f038796561
Stefano <sgazzotti@plum.com.ar>
2022-12-21 14:31:09 -0300

    Add validation for size and extension

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,15 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image, content_type: {
+    in: %w[image/jpeg image/gif image/png],
+    message: "Must be a valid image format"
+  }, size: {
+    less_than: 5.megabytes,
+    message: "Should be less than 5MB"
+  }
+  
 end
